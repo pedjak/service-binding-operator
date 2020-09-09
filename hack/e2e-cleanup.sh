bc085b17e329ab11829d37be3fb7d3b488e1697f
Predrag Knezevic <pknezevi@redhat.com>
2020-09-09 17:10:05 +0200

    e2e-cleanup make rule should not fail if testname does not exist
    * removal of namespace moved to hack/e2e-cleanup.sh
    * if namespace does not exit, do not try to remove it

--- hack/e2e-cleanup.sh
+++ hack/e2e-cleanup.sh
@@ -4,3 +4,6 @@ HACK_DIR=${HACK_DIR:-$(dirname $0)}
 
 # Remove SBR finalizers
 NAMESPACE=$TEST_NAMESPACE $HACK_DIR/remove-sbr-finalizers.sh
+
+
+[ ! -z "$(kubectl get namespace ${TEST_NAMESPACE} --ignore-not-found -o yaml)" ] && kubectl delete namespace ${TEST_NAMESPACE} --timeout=45s --wait || true
