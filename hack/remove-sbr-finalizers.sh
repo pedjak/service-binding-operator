f5493ce314e99b49953613ce2a1fb146f1dcfc99
Pavel Macík <pavel.macik@gmail.com>
2020-09-09 17:19:34 +0200

    remove-sbr-finalizers.sh look for SBRs only if the CRD is in installed.

--- hack/remove-sbr-finalizers.sh
+++ hack/remove-sbr-finalizers.sh
@@ -7,9 +7,12 @@ else
 fi
 
 # Remove SBR finalizers
-SBRS=($(kubectl get sbrs $USE_NS -o jsonpath="{.items[*].metadata.name}"))
-SBR_NS=($(kubectl get sbrs $USE_NS -o jsonpath="{.items[*].metadata.namespace}"))
+SBR_CRD="servicebindingrequests.apps.openshift.io"
+if [[ $(kubectl get crd $SBR_CRD --ignore-not-found) == *"$SBR_CRD"* ]]; then
+    SBRS=($(kubectl get sbrs $USE_NS -o jsonpath="{.items[*].metadata.name}"))
+    SBR_NS=($(kubectl get sbrs $USE_NS -o jsonpath="{.items[*].metadata.namespace}"))
 
-for i in "${!SBRS[@]}"; do
-    kubectl patch sbr/${SBRS[$i]} -p '{"metadata":{"finalizers":[]}}' --type=merge --namespace=${SBR_NS[$i]};
-done
+    for i in "${!SBRS[@]}"; do
+        kubectl patch sbr/${SBRS[$i]} -p '{"metadata":{"finalizers":[]}}' --type=merge --namespace=${SBR_NS[$i]};
+    done
+fi
